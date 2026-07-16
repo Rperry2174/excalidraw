27220012666421be2616f66a9108628dc0738a0b
Ryan Perry <rperry2174@gmail.com>
2026-07-15 20:35:11 -0600

    refactor: simplify SVG label mask and diamond-only roundness
    Remove explicit SVG maskUnits/size for bound-text arrow masks, and
    only recalculate roundness when converting to diamond.

--- packages/excalidraw/components/ConvertElementTypePopup.tsx
+++ packages/excalidraw/components/ConvertElementTypePopup.tsx
@@ -831,13 +831,14 @@ const convertElementType = <
       newElement({
         ...element,
         type: targetType,
-        roundness: element.roundness
-          ? {
-              type: isUsingAdaptiveRadius(targetType)
-                ? ROUNDNESS.ADAPTIVE_RADIUS
-                : ROUNDNESS.PROPORTIONAL_RADIUS,
-            }
-          : element.roundness,
+        roundness:
+          targetType === "diamond" && element.roundness
+            ? {
+                type: isUsingAdaptiveRadius(targetType)
+                  ? ROUNDNESS.ADAPTIVE_RADIUS
+                  : ROUNDNESS.PROPORTIONAL_RADIUS,
+              }
+            : element.roundness,
       }),
     ) as typeof element;
 
